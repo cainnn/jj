5f1aad8b0c950ba3dcd5df99ee3ae4d280090722
cainnn <cainnn@126.com>
2020-04-22 00:23:09 +0800

    1

---
+++ app.js
@@ -0,0 +1,12 @@
+const num1 = 100;
+const num2 = 50;
+
+let val;
+
+val = num1 + num2;
+val = num1 - num2;
+// val = num1 * num2;
+// val = num1 / num2;
+// val = num1 % num2;
+// val = Math.PI;
+console.log(val);
